60704b1644285355d49644dec244cd97038c7a46
KonstantinasArelis <arelis.konstantinas@gmail.com>
2025-04-21 13:03:19 +0300

    Updated ui for image segmentation

--- classificationFrontend/src/App.jsx
+++ classificationFrontend/src/App.jsx
@@ -1,14 +1,36 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import FileInput from './FileInput.jsx'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { BsArrowRight } from "react-icons/bs";
+import { FaSpinner } from 'react-icons/fa';
+
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [imageLabel, setImageLabel] = useState(null);
   const [labelProbability, setLabelProbability] = useState(null);
+  const [imageSegmentationMask, setImageSegmentationMask] = useState(null);
+  const [imageSegmentationLabelData, setImageSegmentationLabelData] = useState(null);
+  const [mode, setMode] = useState("segmentation");
+  const [isLoading, setIsLoading] = useState(false);
   const imageLabelref = useRef(null);
   const labelProbabilityref = useRef(null);
 
+  const handleModeSwitch = () => {
+    setImageLabel(null);
+    setSelectedImage(null);
+    setImageSegmentationLabelData(null);
+    setImageSegmentationMask(null);
+    if (mode === "clasification") {
+      setMode("segmentation");
+    } else {
+      setMode("clasification");
+    }
+  }
 
   const handleFileSelected = (file) => {
     console.log('Selected file:', file);
@@ -17,15 +39,19 @@ function App() {
   };
 
   useEffect(() => {
-    if(selectedImage !== null){
-      handleUpload();
+    if (selectedImage !== null) {
+      if (mode === "clasification") {
+        handleUpload();
+      } else if (mode === "segmentation") {
+        handleUploadSegmentation();
+      }
     }
   }, [selectedImage])
 
   useEffect(() => {
-    if(imageLabelref.current){
+    if (imageLabelref.current) {
       imageLabelref.current.classList.add("imageLabel");
-      
+
       setTimeout(() => {
         imageLabelref.current.classList.remove("imageLabel");
       }, 2000)
@@ -34,15 +60,15 @@ function App() {
 
   useEffect(() => {
     setTimeout(() => {
-      if(labelProbabilityref.current){
-              labelProbabilityref.current.classList.add("labelProbability");
-              setTimeout(() => {
-                labelProbabilityref.current.classList.remove("labelProbability");
-              }, 2000)
-            }
-    },500)
+      if (labelProbabilityref.current) {
+        labelProbabilityref.current.classList.add("labelProbability");
+        setTimeout(() => {
+          labelProbabilityref.current.classList.remove("labelProbability");
+        }, 2000)
+      }
+    }, 500)
   }, [labelProbability])
-  
+
   const handleUpload = async () => {
     if (!selectedImage) {
       alert('Please select an image first.');
@@ -76,33 +102,170 @@ function App() {
     }
   };
 
-  return (
-    <>
-      {imageLabel && (
-        <div>
-          <h1 ref={imageLabelref}>Its a {imageLabel}!</h1>
-          <h3 ref={labelProbabilityref}>{labelProbability} %</h3>
-        </div>
-      )}
-      {!imageLabel && (
-        <h1>Image Classifier</h1>
-      )}
-      <div className="card">
-        
-        <div>
-         <FileInput onFileSelect={handleFileSelected}></FileInput>
-        </div>
-        {selectedImage && (
-          <div>
-          <img className='selectedImage' src={URL.createObjectURL(selectedImage)} alt="Selected"></img>
-        </div>
-        )}
-        {!selectedImage && (<div className='imagePlaceholder'/>)}
-      </div>
+  const handleUploadSegmentation = async () => {
+    if (!selectedImage) {
+      alert('Please select an image first.');
+      return;
+    }
 
-      <footer>Konstantinas Arelis</footer>
-    </>
-  )
+    const formData = new FormData();
+    formData.append('image', selectedImage); // 'image' is the field name the API expects
+    try {
+      setImageSegmentationLabelData(null);
+      setImageSegmentationMask(null);
+      setIsLoading(true);
+      // Make the API call using fetch or another HTTP library (like axios)
+      const response = await fetch('http://localhost:5000/segment', { // Replace '/api/upload' with your actual API endpoint
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log('Image uploaded successfully:', data.segmentation_mask);
+
+        setImageSegmentationMask(data.segmentation_mask);
+        setImageSegmentationLabelData(data.detected_classes);
+      } else {
+        console.error('Error uploading image:', data);
+        alert('Error uploading image.');
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+      alert('Network error occurred.');
+    } finally{
+      setIsLoading(false);
+    }
+  };
+
+  if (mode === 'clasification') {
+    return (
+      <>
+        <header style={{ display: 'flex', position: 'fixed', left: '80%', top: '5%' }}>
+
+
+          <p style={{ marginTop: '8px' }}>Segmentation</p>
+          <Form.Check
+            className='fs-3'
+            type="switch"
+            id="custom-switch"
+            onClick={handleModeSwitch}
+          />
+
+          <p style={{ marginTop: '8px' }}>Clasification</p>
+
+        </header>
+
+
+        {imageLabel && (
+          <div>
+            <h1 ref={imageLabelref}>Its a {imageLabel}!</h1>
+            <h3 ref={labelProbabilityref}>{labelProbability} %</h3>
+          </div>
+        )}
+        {!imageLabel && (
+          <h1>Image {mode}</h1>
+        )}
+        <div>
+
+          <div>
+            <FileInput onFileSelect={handleFileSelected}></FileInput>
+          </div>
+          {selectedImage && (
+            <div>
+              <img className='selectedImage' src={URL.createObjectURL(selectedImage)} alt="Selected"></img>
+            </div>
+          )}
+          {!selectedImage && (<div className='imagePlaceholder' />)}
+        </div>
+
+          
+
+
+
+        <footer>Konstantinas Arelis</footer>
+      </>
+    )
+  } else if (mode === 'segmentation') {
+    return (
+      <>
+        <header style={{ display: 'flex', position: 'fixed', left: '80%', top: '5%' }}>
+
+
+          <p style={{ marginTop: '8px' }}>Segmentation</p>
+          <Form.Check
+            className='fs-3'
+            type="switch"
+            id="custom-switch"
+            onClick={handleModeSwitch}
+          />
+
+          <p style={{ marginTop: '8px' }}>Clasification</p>
+
+        </header>
+
+        <h1>Image {mode}</h1>
+
+        <div>
+
+          <div>
+            <FileInput onFileSelect={handleFileSelected}></FileInput>
+          </div>
+
+          <Container>
+            <Row style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <Col>
+                {selectedImage && (
+                  <div>
+                    <img className='selectedImage' src={URL.createObjectURL(selectedImage)} alt="Selected"></img>
+                  </div>
+                )}
+                {!selectedImage && (<div className='imagePlaceholder' />)}
+              </Col>
+              <Col>
+                <BsArrowRight size={"200px"}/>
+
+              </Col>
+              <Col>
+                {imageSegmentationMask && imageSegmentationLabelData && (
+                  <div>
+                    <img src={imageSegmentationMask}></img>
+
+                    {imageSegmentationLabelData.map((d) => {
+                      return (
+                        <div key={d.index}>
+                          <Container style={{margin: '10px'}}>
+                            <Row>
+                              <Col>
+                                <div style={{aspectRatio: '1', width: '30px', backgroundColor: `rgb(${d.color[0]}, ${d.color[1]}, ${d.color[2]})` }}>
+                                  
+                                </div>
+                              </Col>
+                              <Col>
+                                {d.name}
+                              </Col>
+                            </Row>
+                          </Container>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )}
+                {! isLoading && !imageSegmentationMask && !imageSegmentationLabelData && (
+                  <div className='imagePlaceholder' />
+                )}
+                {isLoading && (
+                  <FaSpinner size={"200px"} className="icon-spin" />
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <footer>Konstantinas Arelis</footer>
+      </>
+    )
+  }
 }
 
 export default App
